93ddefa66c14e550efbaba8bdc2231f8dcef41ce
adam <adaminiature@gmail.com>
2023-09-22 16:17:41 +0700

    update next config

--- next.config.js
+++ next.config.js
@@ -1,12 +1,8 @@
 /** @type {import('next').NextConfig} */
-const path = require("path");
 
 const nextConfig = {
   reactStrictMode: true,
   // basePath: '/kukeight_static',
-  sassOptions: {
-    includePaths: [path.join(__dirname, "styles")],
-  },
 };
 
 module.exports = nextConfig;
