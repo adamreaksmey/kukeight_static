e1bbc887781c9d15e4ddde9d5b8eaa1b783bc3e9
adamreaksmey <adaminiature@gmail.com>
2023-10-04 12:03:01 +0700

    removed unused code

--- src/pages/signup/index.js
+++ src/pages/signup/index.js
@@ -3,17 +3,6 @@ import React, { useState } from "react";
 import Signup from "@/components/top/signup";
 
 export default function SignupPage() {
-  const [form, setForm] = useState({
-    email: "",
-    username: "",
-    age: "",
-    country: "",
-    password: "",
-    confirm_password: "",
-    language: "",
-    user_type: "",
-  });
-
   return (
     <>
       <BasicLayout>
