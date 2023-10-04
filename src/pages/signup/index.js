8e919c649fb9dc055856de86af398c81b333994e
adamreaksmey <adaminiature@gmail.com>
2023-10-04 12:02:00 +0700

    removed unused code

--- src/pages/signup/index.js
+++ src/pages/signup/index.js
@@ -1,7 +1,6 @@
 import { BasicLayout } from "@/layout/basic";
-import React, { use, useState } from "react";
+import React, { useState } from "react";
 import Signup from "@/components/top/signup";
-import { Form, Button, Row, Col } from "react-bootstrap";
 
 export default function SignupPage() {
   const [form, setForm] = useState({
@@ -14,20 +13,6 @@ export default function SignupPage() {
     language: "",
     user_type: "",
   });
-
-  const handleSubmit = (e) => {
-    e.preventDefault();
-    console.log(form);
-  };
-
-  const handleFormChange = (e) => {
-    const { name, value } = e.target;
-    console.log({ name: name, value: value });
-    setForm((prevFormData) => ({
-      ...prevFormData,
-      [name]: value,
-    }));
-  };
 
   return (
     <>
