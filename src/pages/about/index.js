8f5a76159bc61aacc72eac2c5f54d1c39f96f2c6
adamreaksmey <adaminiature@gmail.com>
2023-10-06 14:16:32 +0700

    about pages

--- src/pages/about/index.js
+++ src/pages/about/index.js
@@ -1,120 +1,31 @@
-import { useEffect, useState } from "react";
-import { MainLayout } from "@/layout/index";
 import { BasicLayout } from "@/layout/basic";
+import Creators from "@/components/about/creators";
 
 const About = () => {
-  const [scrollPosition, setScrollPosition] = useState(0);
-
-  useEffect(() => {
-    const handleScroll = () => {
-      const position = window.scrollY;
-      setScrollPosition(position);
-    };
-
-    handleScroll(); // Set initial scroll position on the client side
-
-    window.addEventListener("scroll", handleScroll);
-
-    return () => {
-      window.removeEventListener("scroll", handleScroll);
-    };
-  }, []);
-
   return (
-    <MainLayout>
-      <div>
-        {/* Child component content */}
-        <p>Scroll position: {scrollPosition}</p>
-        <p>Scroll position: {scrollPosition}</p>
-        <p>Scroll position: {scrollPosition}</p>
-        <p>Scroll position: {scrollPosition}</p>
-        <p>Scroll position: {scrollPosition}</p>
-        <p>Scroll position: {scrollPosition}</p>
-        <p>Scroll position: {scrollPosition}</p>
-        <p>Scroll position: {scrollPosition}</p>
-
-        <p>Scroll position: {scrollPosition}</p>
-        <p>Scroll position: {scrollPosition}</p>
-        <p>Scroll position: {scrollPosition}</p>
-        <p>Scroll position: {scrollPosition}</p>
-        <p>Scroll position: {scrollPosition}</p>
-        <p>Scroll position: {scrollPosition}</p>
-        <p>Scroll position: {scrollPosition}</p>
-        <p>Scroll position: {scrollPosition}</p>
-        <p>Scroll position: {scrollPosition}</p>
-        <p>Scroll position: {scrollPosition}</p>
-        <p>Scroll position: {scrollPosition}</p>
-        <p>Scroll position: {scrollPosition}</p>
-        <p>Scroll position: {scrollPosition}</p>
-        <p>Scroll position: {scrollPosition}</p>
-        <p>Scroll position: {scrollPosition}</p>
-        <p>Scroll position: {scrollPosition}</p>
-        <p>Scroll position: {scrollPosition}</p>
-        <p>Scroll position: {scrollPosition}</p>
-        <p>Scroll position: {scrollPosition}</p>
-        <p>Scroll position: {scrollPosition}</p>
-        <p>Scroll position: {scrollPosition}</p>
-        <p>Scroll position: {scrollPosition}</p>
-        <p>Scroll position: {scrollPosition}</p>
-        <p>Scroll position: {scrollPosition}</p>
-        <p>Scroll position: {scrollPosition}</p>
-        <p>Scroll position: {scrollPosition}</p>
-        <p>Scroll position: {scrollPosition}</p>
-        <p>Scroll position: {scrollPosition}</p>
-        <p>Scroll position: {scrollPosition}</p>
-        <p>Scroll position: {scrollPosition}</p>
-        <p>Scroll position: {scrollPosition}</p>
-        <p>Scroll position: {scrollPosition}</p>
-        <p>Scroll position: {scrollPosition}</p>
-        <p>Scroll position: {scrollPosition}</p>
-        <p>Scroll position: {scrollPosition}</p>
-        <p>Scroll position: {scrollPosition}</p>
-        <p>Scroll position: {scrollPosition}</p>
-        <p>Scroll position: {scrollPosition}</p>
-        <p>Scroll position: {scrollPosition}</p>
-        <p>Scroll position: {scrollPosition}</p>
-        <p>Scroll position: {scrollPosition}</p>
-        <p>Scroll position: {scrollPosition}</p>
-        <p>Scroll position: {scrollPosition}</p>
-        <p>Scroll position: {scrollPosition}</p>
-        <p>Scroll position: {scrollPosition}</p>
-        <p>Scroll position: {scrollPosition}</p>
-        <p>Scroll position: {scrollPosition}</p>
-        <p>Scroll position: {scrollPosition}</p>
-        <p>Scroll position: {scrollPosition}</p>
-        <p>Scroll position: {scrollPosition}</p>
-        <p>Scroll position: {scrollPosition}</p>
-        <p>Scroll position: {scrollPosition}</p>
-        <p>Scroll position: {scrollPosition}</p>
-        <p>Scroll position: {scrollPosition}</p>
-        <p>Scroll position: {scrollPosition}</p>
-        <p>Scroll position: {scrollPosition}</p>
-        <p>Scroll position: {scrollPosition}</p>
-        <p>Scroll position: {scrollPosition}</p>
-        <p>Scroll position: {scrollPosition}</p>
-        <p>Scroll position: {scrollPosition}</p>
-        <p>Scroll position: {scrollPosition}</p>
-        <p>Scroll position: {scrollPosition}</p>
-        <p>Scroll position: {scrollPosition}</p>
-        <p>Scroll position: {scrollPosition}</p>
-        <p>Scroll position: {scrollPosition}</p>
-        <p>Scroll position: {scrollPosition}</p>
-        <p>Scroll position: {scrollPosition}</p>
-        <p>Scroll position: {scrollPosition}</p>
-        <p>Scroll position: {scrollPosition}</p>
-        <p>Scroll position: {scrollPosition}</p>
-        <p>Scroll position: {scrollPosition}</p>
-        <p>Scroll position: {scrollPosition}</p>
-
-        <p>Scroll position: {scrollPosition}</p>
-        <p>Scroll position: {scrollPosition}</p>
-        <p>Scroll position: {scrollPosition}</p>
-        <p>Scroll position: {scrollPosition}</p>
-        <p>Scroll position: {scrollPosition}</p>
-        <p>Scroll position: {scrollPosition}</p>
-        {/* Additional child component elements */}
+    <BasicLayout>
+      <div className="text-center w-100 p-5">
+        <h1 className="fw-bold">
+          For the greatest minds, By the best creators!
+        </h1>
+        <div className="w-100 d-flex justify-content-center pb-4">
+          <hr className="w-50" />
+        </div>
+        <div className="w-100 d-flex justify-content-center pb-5">
+          <div className="w-50 text-center">
+            Our small team, comprising four visionary co-founders, is driven by
+            unwavering passion and profound expertise. We pride ourselves on
+            delivering unrivaled excellence to writers and novelists, harnessing
+            the power of cutting-edge artificial intelligence. Our mission is to
+            keep you captivated, fully immersed in the art of reading and
+            writing like never before!
+          </div>
+        </div>
+        <div className="text-start">
+          <Creators />
+        </div>
       </div>
-    </MainLayout>
+    </BasicLayout>
   );
 };
 
