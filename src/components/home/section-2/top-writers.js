dcc6c04fd15fb65317090324cfe6b2f52e92924e
adamreaksmey <adaminiature@gmail.com>
2023-10-10 10:19:43 +0700

    added top writers section

--- src/components/home/section-2/top-writers.js
+++ src/components/home/section-2/top-writers.js
@@ -5,8 +5,8 @@ const TopWriters = (props) => {
   const { writers } = props;
   return (
     <>
-      {writers.slice(0, 5).map((data, index) => (
-        <div className="d-flex w-100 gap-2">
+      {writers.slice(0, 6).map((data, index) => (
+        <div className="d-flex w-100 gap-2 pb-4">
           <Image
             src={data.image_url}
             width={60}
@@ -14,8 +14,9 @@ const TopWriters = (props) => {
             className="logo-img"
             alt={data.name}
           />
-          <div style={{ fontSize: "0.9em" }}>
-            <div>{data.name}</div>
+          <div style={{ fontSize: "0.9em" }} className="w-100">
+            <div className="pb-1">{data.name}</div>
+            <div style={{ fontSize: "0.8em" }}>{data.most_read_book}</div>
           </div>
         </div>
       ))}
