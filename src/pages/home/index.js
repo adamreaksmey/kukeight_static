21c428d23c7b32c49e39e468bcdffec2b683f6fa
adamreaksmey <adaminiature@gmail.com>
2023-10-10 12:59:20 +0700

    added book of the year

--- src/pages/home/index.js
+++ src/pages/home/index.js
@@ -8,6 +8,9 @@ import ContinueReading from "@/components/home/section-2/continue-reading";
 import { Row, Col } from "react-bootstrap";
 import TopWritersList from "@/components/functions/home/writers";
 import TopWriters from "@/components/home/section-2/top-writers";
+import SectionThree from "@/components/home/section-3";
+import WS from "@/public/photos/writers/WS.jpeg";
+import BookOfTheYear from "@/public/photos/recommendation/fault-in-our-stars.jpeg";
 
 const Home = () => {
   const [recommendedBooks, setRecommendedBooks] = useState([]);
@@ -20,6 +23,12 @@ const Home = () => {
     const setWriters = TopWritersList();
     setTopWriters(setWriters);
   }, []);
+
+  const writerOfTheYearDescription =
+    "William Shakespeare, the renowned playwright of the Elizabethan era, continues to captivate audiences worldwide with his timeless works. His profound insights into the human condition and masterful storytelling make him an enduring literary icon.";
+
+  const bookOfTheYearDescription =
+    "The Fault in Our Stars by John Green is a luminous tapestry of love and resilience, painted with delicate strokes of hope and heartache. Through the tender bond of Hazel and Gus, it explores the fragility of existence and the transformative power of embracing life's fleeting moments.";
   return (
     <>
       <Paper>
@@ -46,9 +55,26 @@ const Home = () => {
             <TopWriters writers={topWriters} />
           </Col>
         </Row>
-        <Row className="section-2 continue-reading saved-books pt-5 gap-2">
+        <Row className="section-3 writer-of-the-year pt-5 gap-2">
+          <Col xs={5} className="books-grid-padding h-100">
+            <div className="px-5">
+              <h5 className="pb-3 pt-3 fw-bold">Writer Of The Year</h5>
+              <SectionThree
+                image={WS.src}
+                description={writerOfTheYearDescription}
+                buttonCaption={"View writer's work..."}
+              />
+            </div>
+          </Col>
           <Col xs={5} className="books-grid-padding">
-            <h5 className="px-5 pb-3 pt-3 fw-bold">Writer Of The Year</h5>
+            <div className="px-5">
+              <h5 className="pb-3 pt-3 fw-bold">Book Of The Year</h5>
+              <SectionThree
+                image={BookOfTheYear.src}
+                description={bookOfTheYearDescription}
+                buttonCaption={"View writer's work..."}
+              />
+            </div>
           </Col>
         </Row>
       </Paper>
