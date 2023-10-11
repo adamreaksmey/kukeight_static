564321a0ee7cbb93e0c323a62102be2b072c5009
adamreaksmey <adaminiature@gmail.com>
2023-10-11 13:28:48 +0700

    profile page init

---
+++ src/pages/profile/index.js
@@ -0,0 +1,14 @@
+import React from "react";
+import Paper from "@/layout/paper";
+
+const Profile = () => {
+  return (
+    <>
+      <Paper>
+        <div>hello world</div>
+      </Paper>
+    </>
+  );
+};
+
+export default Profile;
