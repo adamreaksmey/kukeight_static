c8d7ef56fb80a0cfe5d2d7661300fe1d1bc28ced
adamreaksmey <adaminiature@gmail.com>
2023-10-12 12:59:28 +0700

    title font

--- src/components/profile/sections/profile.js
+++ src/components/profile/sections/profile.js
@@ -1,18 +1,92 @@
-import React from "react";
-import { Row, Col } from "react-bootstrap";
+import React, { useState } from "react";
+import { Row, Col, Form } from "react-bootstrap";
 
 const EditProfile = () => {
+  const [form, setForm] = useState({
+    email: "",
+    username: "",
+    age: "",
+    country: "",
+    password: "",
+    confirm_password: "",
+    language: "",
+    user_type: "",
+    user_is_loggedout: false,
+  });
+
+  const handleFormChange = (e) => {
+    const { name, value } = e.target;
+    setForm((prevFormData) => ({
+      ...prevFormData,
+      [name]: value,
+    }));
+  };
+
   return (
     <div>
       <Row
         className="p-4 rounded mb-1 logo-img"
         style={{ background: "#6A9C89" }}
       >
-        <div>
-            <h5 className="fw-bold text-decoration-underline">Edit your info below here!</h5>
+        <div className="pb-4">
+          <h5 className="fw-bold text-decoration-underline title-font">
+            Edit your info below here!
+          </h5>
         </div>
-        <Col>Hello world</Col>
-        <Col>Hello world</Col>
+        <Form>
+          <Row>
+            <Col>
+              <Form.Group controlId="name">
+                <Form.Label>Userame:</Form.Label>
+                <Form.Control
+                  name="username"
+                  type="text"
+                  value={form.username}
+                  onChange={handleFormChange}
+                  required
+                />
+              </Form.Group>
+            </Col>
+            <Col>
+              <Form.Group controlId="name">
+                <Form.Label>Email Address:</Form.Label>
+                <Form.Control
+                  name="email"
+                  type="email"
+                  value={form.email}
+                  onChange={handleFormChange}
+                  required
+                />
+              </Form.Group>
+            </Col>
+          </Row>
+          <Row>
+            <Col>
+              <Form.Group controlId="name">
+                <Form.Label>Language:</Form.Label>
+                <Form.Control
+                  name="language"
+                  type="email"
+                  value={form.username}
+                  onChange={handleFormChange}
+                  required
+                />
+              </Form.Group>
+            </Col>
+            <Col>
+              <Form.Group controlId="name">
+                <Form.Label>Email Address:</Form.Label>
+                <Form.Control
+                  name="email"
+                  type="email"
+                  value={form.email}
+                  onChange={handleFormChange}
+                  required
+                />
+              </Form.Group>
+            </Col>
+          </Row>
+        </Form>
       </Row>
     </div>
   );
