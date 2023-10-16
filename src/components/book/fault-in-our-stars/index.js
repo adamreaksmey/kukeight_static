bbe288a5a8fc71979d6683fc28961ed306ad12a1
adamreaksmey <adaminiature@gmail.com>
2023-10-16 08:58:48 +0700

    fios update

--- src/components/book/fault-in-our-stars/index.js
+++ src/components/book/fault-in-our-stars/index.js
@@ -206,43 +206,51 @@ const TheFaultInOurStars = () => {
       image: CoverTheFaultInOurStars.src,
     },
     {
-      page_content: `Late in the winter of my seventeenth year, my mother decided I was depressed,
-            presumably because I rarely left the house, spent quite a lot of time in bed, read the same
-            book over and over, ate infrequently, and devoted quite a bit of my abundant free time to
-            thinking about death.
-            Whenever you read a cancer booklet or website or whatever, they always list
-            depression among the side effects of cancer. But, in fact, depression is not a side effect of
-            cancer. Depression is a side effect of dying. (Cancer is also a side effect of dying. Almost
-            everything is, really.) But my mom believed I required treatment, so she took me to see
-            my Regular Doctor Jim, who agreed that I was veritably swimming in a paralyzing and
-            totally clinical depression, and that therefore my meds should be adjusted and also I
-            should attend a weekly Support Group.
-            This Support Group featured a rotating cast of characters in various states of tumordriven unwellness. Why did the cast rotate? A side effect of dying.
-            The Support Group, of course, was depressing as hell. It met every Wednesday in the
-            basement of a stone-walled Episcopal church shaped like a cross. We all sat in a circle
-            right in the middle of the cross, where the two boards would have met, where the heart of
-            Jesus would have been.
-            I noticed this because Patrick, the Support Group Leader and only person over
-            eighteen in the room, talked about the heart of Jesus every freaking meeting, all about how
-            we, as young cancer survivors, were sitting right in Christ’s very sacred heart and
-            whatever.
-            So here’s how it went in God’s heart: The six or seven or ten of us walked/wheeled
-            in, grazed at a decrepit selection of cookies and lemonade, sat down in the Circle of Trust,
-            and listened to Patrick recount for the thousandth time his depressingly miserable life
-            story—how he had cancer in his balls and they thought he was going to die but he didn’t
-            die and now here he is, a full-grown adult in a church basement in the 137th nicest city in
-            America, divorced, addicted to video games, mostly friendless, eking out a meager living
-            by exploiting his cancertastic past, slowly working his way toward a master’s degree that
-            will not improve his career prospects, waiting, as we all do, for the sword of Damocles to
-            give him the relief that he escaped lo those many years ago when cancer took both of his
-            nuts but spared what only the most generous soul would call his life.
-            AND YOU TOO MIGHT BE SO LUCKY!
-            Then we introduced ourselves: Name. Age. Diagnosis. And how we’re doing today.
-            I’m Hazel, I’d say when they’d get to me. Sixteen. Thyroid originally but with an
-            impressive and long-settled satellite colony in my lungs. And I’m doing okay.
-            Once we got around the circle, Patrick always asked if anyone wanted to share. And
-            then began the circle jerk of support: everyone talking about fighting and battling and
-            winning and shrinking and scanning. To be fair to Patrick, he let us talk about dying, too.`,
+      page_content: `Neither of us said anything for the rest of Support Group. At the end, we all had to
+      hold hands, and Patrick led us in a prayer. “Lord Jesus Christ, we are gathered here in
+      Your heart, literally in Your heart, as cancer survivors. You and You alone know us as we
+      know ourselves. Guide us to life and the Light through our times of trial. We pray for
+      Isaac’s eyes, for Michael’s and Jamie’s blood, for Augustus’s bones, for Hazel’s lungs, for
+      James’s throat. We pray that You might heal us and that we might feel Your love, and Your
+      peace, which passes all understanding. And we remember in our hearts those whom we
+      knew and loved who have gone home to you: Maria and Kade and Joseph and Haley and
+      Abigail and Angelina and Taylor and Gabriel and . . .”
+      It was a long list. The world contains a lot of dead people. And while Patrick droned
+      on, reading the list from a sheet of paper because it was too long to memorize, I kept my
+      eyes closed, trying to think prayerfully but mostly imagining the day when my name
+      would find its way onto that list, all the way at the end when everyone had stopped
+      listening.
+      When Patrick was finished, we said this stupid mantra together—LIVING OUR
+      BEST LIFE TODAY—and it was over. Augustus Waters pushed himself out of his chair
+      and walked over to me. His gait was crooked like his smile. He towered over me, but he
+      kept his distance so I wouldn’t have to crane my neck to look him in the eye. “What’s your
+      name?” he asked.
+      “Hazel.”
+      “No, your full name.”
+      “Um, Hazel Grace Lancaster.” He was just about to say something else when Isaac
+      walked up. “Hold on,” Augustus said, raising a finger, and turned to Isaac. “That was
+      actually worse than you made it out to be.”
+      “I told you it was bleak.”
+      “Why do you bother with it?”
+      “I don’t know. It kind of helps?”
+      Augustus leaned in so he thought I couldn’t hear. “She’s a regular?” I couldn’t hear
+      Isaac’s comment, but Augustus responded, “I’ll say.” He clasped Isaac by both shoulders
+      and then took a half step away from him. “Tell Hazel about clinic.”
+      Isaac leaned a hand against the snack table and focused his huge eye on me. “Okay,
+      so I went into clinic this morning, and I was telling my surgeon that I’d rather be deaf than
+      blind. And he said, ‘It doesn’t work that way,’ and I was, like, ‘Yeah, I realize it doesn’t
+      work that way; I’m just saying I’d rather be deaf than blind if I had the choice, which I
+      realize I don’t have,’ and he said, ‘Well, the good news is that you won’t be deaf,’ and I
+      was like, ‘Thank you for explaining that my eye cancer isn’t going to make me deaf. I feel
+      so fortunate that an intellectual giant like yourself would deign to operate on me.’”
+      “He sounds like a winner,” I said. “I’m gonna try to get me some eye cancer just so I
+      can make this guy’s acquaintance.”
+      “Good luck with that. All right, I should go. Monica’s waiting for me. I gotta look at
+      her a lot while I can.”
+      “Counterinsurgence tomorrow?” Augustus asked.
+      “Definitely.” Isaac turned and ran up the stairs, taking them two at a time.
+      Augustus Waters turned to me. “Literally,” he said.
+      “Literally?” I asked.`,
       image: CoverTheFaultInOurStars.src,
     },
   ];
