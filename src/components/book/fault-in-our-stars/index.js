416ae23c6ed287ea44d2bfb1ebb309dbbccfeef2
adamreaksmey <adaminiature@gmail.com>
2023-10-16 08:55:44 +0700

    fios update

--- src/components/book/fault-in-our-stars/index.js
+++ src/components/book/fault-in-our-stars/index.js
@@ -83,83 +83,48 @@ const TheFaultInOurStars = () => {
       image: CoverTheFaultInOurStars.src,
     },
     {
-      page_content: `Late in the winter of my seventeenth year, my mother decided I was depressed,
-            presumably because I rarely left the house, spent quite a lot of time in bed, read the same
-            book over and over, ate infrequently, and devoted quite a bit of my abundant free time to
-            thinking about death.
-            Whenever you read a cancer booklet or website or whatever, they always list
-            depression among the side effects of cancer. But, in fact, depression is not a side effect of
-            cancer. Depression is a side effect of dying. (Cancer is also a side effect of dying. Almost
-            everything is, really.) But my mom believed I required treatment, so she took me to see
-            my Regular Doctor Jim, who agreed that I was veritably swimming in a paralyzing and
-            totally clinical depression, and that therefore my meds should be adjusted and also I
-            should attend a weekly Support Group.
-            This Support Group featured a rotating cast of characters in various states of tumordriven unwellness. Why did the cast rotate? A side effect of dying.
-            The Support Group, of course, was depressing as hell. It met every Wednesday in the
-            basement of a stone-walled Episcopal church shaped like a cross. We all sat in a circle
-            right in the middle of the cross, where the two boards would have met, where the heart of
-            Jesus would have been.
-            I noticed this because Patrick, the Support Group Leader and only person over
-            eighteen in the room, talked about the heart of Jesus every freaking meeting, all about how
-            we, as young cancer survivors, were sitting right in Christ’s very sacred heart and
-            whatever.
-            So here’s how it went in God’s heart: The six or seven or ten of us walked/wheeled
-            in, grazed at a decrepit selection of cookies and lemonade, sat down in the Circle of Trust,
-            and listened to Patrick recount for the thousandth time his depressingly miserable life
-            story—how he had cancer in his balls and they thought he was going to die but he didn’t
-            die and now here he is, a full-grown adult in a church basement in the 137th nicest city in
-            America, divorced, addicted to video games, mostly friendless, eking out a meager living
-            by exploiting his cancertastic past, slowly working his way toward a master’s degree that
-            will not improve his career prospects, waiting, as we all do, for the sword of Damocles to
-            give him the relief that he escaped lo those many years ago when cancer took both of his
-            nuts but spared what only the most generous soul would call his life.
-            AND YOU TOO MIGHT BE SO LUCKY!
-            Then we introduced ourselves: Name. Age. Diagnosis. And how we’re doing today.
-            I’m Hazel, I’d say when they’d get to me. Sixteen. Thyroid originally but with an
-            impressive and long-settled satellite colony in my lungs. And I’m doing okay.
-            Once we got around the circle, Patrick always asked if anyone wanted to share. And
-            then began the circle jerk of support: everyone talking about fighting and battling and
-            winning and shrinking and scanning. To be fair to Patrick, he let us talk about dying, too.`,
+      page_content: `That shut me up, although I failed to see how attendance at Support Group met the
+      definition of life. Still, I agreed to go—after negotiating the right to record the 1.5
+      episodes of ANTM I’d be missing.
+      I went to Support Group for the same reason that I’d once allowed nurses with a mere
+      eighteen months of graduate education to poison me with exotically named chemicals: I
+      wanted to make my parents happy. There is only one thing in this world shittier than biting
+      it from cancer when you’re sixteen, and that’s having a kid who bites it from cancer.
+      Mom pulled into the circular driveway behind the church at 4:56. I pretended to fiddle
+      with my oxygen tank for a second just to kill time.
+      “Do you want me to carry it in for you?”
+      “No, it’s fine,” I said. The cylindrical green tank only weighed a few pounds, and I
+      had this little steel cart to wheel it around behind me. It delivered two liters of oxygen to
+      me each minute through a cannula, a transparent tube that split just beneath my neck,
+      wrapped behind my ears, and then reunited in my nostrils. The contraption was necessary
+      because my lungs sucked at being lungs.
+      “I love you,” she said as I got out.
+      “You too, Mom. See you at six.”
+      “Make friends!” she said through the rolled-down window as I walked away.
+      I didn’t want to take the elevator because taking the elevator is a Last Days kind of
+      activity at Support Group, so I took the stairs. I grabbed a cookie and poured some
+      lemonade into a Dixie cup and then turned around.
+      A boy was staring at me.
+      I was quite sure I’d never seen him before. Long and leanly muscular, he dwarfed the
+      molded plastic elementary school chair he was sitting in. Mahogany hair, straight and
+      short. He looked my age, maybe a year older, and he sat with his tailbone against the edge
+      of the chair, his posture aggressively poor, one hand half in a pocket of dark jeans.
+      I looked away, suddenly conscious of my myriad insufficiencies. I was wearing old
+      jeans, which had once been tight but now sagged in weird places, and a yellow T-shirt
+      advertising a band I didn’t even like anymore. Also my hair: I had this pageboy haircut,
+      and I hadn’t even bothered to, like, brush it. Furthermore, I had ridiculously fat
+      chipmunked cheeks, a side effect of treatment. I looked like a normally proportioned
+      person with a balloon for a head. This was not even to mention the cankle situation. And
+      yet—I cut a glance to him, and his eyes were still on me.
+      It occurred to me why they call it eye contact.
+      I walked into the circle and sat down next to Isaac, two seats away from the boy. I
+      glanced again. He was still watching me.
+      Look, let me just say it: He was hot. A nonhot boy stares at you relentlessly and it is,
+      at best, awkward and, at worst, a form of assault. But a hot boy . . . well.`,
       image: CoverTheFaultInOurStars.src,
     },
     {
-      page_content: `Late in the winter of my seventeenth year, my mother decided I was depressed,
-            presumably because I rarely left the house, spent quite a lot of time in bed, read the same
-            book over and over, ate infrequently, and devoted quite a bit of my abundant free time to
-            thinking about death.
-            Whenever you read a cancer booklet or website or whatever, they always list
-            depression among the side effects of cancer. But, in fact, depression is not a side effect of
-            cancer. Depression is a side effect of dying. (Cancer is also a side effect of dying. Almost
-            everything is, really.) But my mom believed I required treatment, so she took me to see
-            my Regular Doctor Jim, who agreed that I was veritably swimming in a paralyzing and
-            totally clinical depression, and that therefore my meds should be adjusted and also I
-            should attend a weekly Support Group.
-            This Support Group featured a rotating cast of characters in various states of tumordriven unwellness. Why did the cast rotate? A side effect of dying.
-            The Support Group, of course, was depressing as hell. It met every Wednesday in the
-            basement of a stone-walled Episcopal church shaped like a cross. We all sat in a circle
-            right in the middle of the cross, where the two boards would have met, where the heart of
-            Jesus would have been.
-            I noticed this because Patrick, the Support Group Leader and only person over
-            eighteen in the room, talked about the heart of Jesus every freaking meeting, all about how
-            we, as young cancer survivors, were sitting right in Christ’s very sacred heart and
-            whatever.
-            So here’s how it went in God’s heart: The six or seven or ten of us walked/wheeled
-            in, grazed at a decrepit selection of cookies and lemonade, sat down in the Circle of Trust,
-            and listened to Patrick recount for the thousandth time his depressingly miserable life
-            story—how he had cancer in his balls and they thought he was going to die but he didn’t
-            die and now here he is, a full-grown adult in a church basement in the 137th nicest city in
-            America, divorced, addicted to video games, mostly friendless, eking out a meager living
-            by exploiting his cancertastic past, slowly working his way toward a master’s degree that
-            will not improve his career prospects, waiting, as we all do, for the sword of Damocles to
-            give him the relief that he escaped lo those many years ago when cancer took both of his
-            nuts but spared what only the most generous soul would call his life.
-            AND YOU TOO MIGHT BE SO LUCKY!
-            Then we introduced ourselves: Name. Age. Diagnosis. And how we’re doing today.
-            I’m Hazel, I’d say when they’d get to me. Sixteen. Thyroid originally but with an
-            impressive and long-settled satellite colony in my lungs. And I’m doing okay.
-            Once we got around the circle, Patrick always asked if anyone wanted to share. And
-            then began the circle jerk of support: everyone talking about fighting and battling and
-            winning and shrinking and scanning. To be fair to Patrick, he let us talk about dying, too.`,
+      page_content: ``,
       image: CoverTheFaultInOurStars.src,
     },
     {
